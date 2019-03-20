14d2afe817a2d6ae36924878704750bba19d3810
DimaGashko <dgashko@mail.ua>
2019-03-20 10:09:17 +0200

    addNewBOokCompent

--- src/com/models/Book.java
+++ src/com/models/Book.java
@@ -121,3 +121,141 @@ public class Book implements Serializable  {
         return new Book(name, author, publisher, date, pages, price);
     }
 }
+
+
+/*
+
+
+
+
+
+
+
+
+
+package com.labs.lab_s4_3;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Book implements Comparable<Book>, Serializable {
+    private int id;
+    private String name;
+    private String author;
+    private String publisher;
+    private LocalDate publishDate;
+    private int pages;
+    private double price;
+
+    public Book(int id, String name, String author, String publisher, LocalDate publishDate, int pages, double price) {
+        setId(id);
+        setName(name);
+        setAuthor(author);
+        setPublisher(publisher);
+        setPublishDate(publishDate);
+        setPages(pages);
+        setPrice(price);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", publishDate=" + publishDate +
+                ", pages=" + pages +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public int compareTo(@NotNull Book o) {
+        return Comparator.comparing(Book::getId, Integer::compareTo).compare(this, o);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+* */
